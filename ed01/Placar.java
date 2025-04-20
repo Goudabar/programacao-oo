3d8b34f5bc5962425fcec6ab14154ca40811d2fe
Goudabar <leanzee@leanzee.com>
2025-04-19 21:31:02 -0300

    Create Placar.java

---
+++ ed01/Placar.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Classe para armazenar e mostrar ranking dos jogadores.
+ */
+public class Placar
+  {
+    private ArrayList<Jogador> ranking;
+
+    /**
+     * Inicializa o ranking.
+     */
+    public Placar()
+    {
+        ranking = new ArrayList<>();
+    }
+
+    /*
+     * Adiciona um jogador ao ranking.
+     */
+    public void adicionarJogador(Jogador j)
+    {
+        ranking.add(j);
+    }
+
+    /*
+     * Exibe o ranking com nome e pontuação dos jogadores.
+     */
+    public void mostrarRanking()
+    {
+        System.out.println("\nRanking:");
+        Collections.sort(ranking, Comparator.comparingInt(Jogador::getPontuacao).reversed());
+        for (Jogador j : ranking)
+        {
+            System.out.println(j.getNome() + " - Pontuação: " + j.getPontuacao());
+        }
+    }
+}
