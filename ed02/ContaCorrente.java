199fb645ca6f52ccc6a261bb47c76ef249663b2c
Goudabar <fabinbin7.000@gmail.com>
2025-05-16 17:28:31 -0300

    Adicionando o código da  Classe 'ContaCorrente.java'

---
+++ ed02/ContaCorrente.java
@@ -0,0 +1,26 @@
+package ed02;
+
+/**
+ * Conta corrente, que aplica 0.1% de juros ao dia
+ */
+public class ContaCorrente extends Conta {
+
+    /**
+     * Cria a conta corrente com nome do cliente e saldo inicial
+     */
+    public ContaCorrente(String cliente, double saldo) {
+        super(cliente, saldo);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Saldo: R$" + saldo);
+    }
+
+    @Override
+    public void aplicarJurosDiarios() {
+        saldo += saldo * 0.001;
+    }
+}
