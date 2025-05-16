4dc609d282f401523f59f1c06086f4483248eb57
Goudabar <fabinbin7.000@gmail.com>
2025-05-16 17:37:14 -0300

    Adicionando a classe 'ContaSalario.java'

---
+++ ed02/ContaSalario.java
@@ -0,0 +1,26 @@
+package ed02;
+
+/**
+ * Conta salário, que não aplica juros diários
+ */
+public class ContaSalario extends Conta {
+
+    /**
+     * Criar conta salário com nome do cliente e saldo inicial
+     */
+    public ContaSalario(String cliente, double saldo) {
+        super(cliente, saldo);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Salário ===");
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Saldo: R$" + saldo);
+    }
+
+    @Override
+    public void aplicarJurosDiarios() {
+        // Conta salário não recebe juros.
+    }
+}
